3884da9e4d0a54fc62e6ed44f6e238afe1b1b918
Nicolas Guebel <nicolas.guebel@gmail.com>
2022-10-13 23:10:40 +0200

    first running version ml pipeline

--- app/run.py
+++ app/run.py
@@ -1,48 +1,76 @@
 import json
 import plotly
 import pandas as pd
+import numpy as np
+import string
+import re
 
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 
 from flask import Flask
 from flask import render_template, request, jsonify
-from plotly.graph_objs import Bar
-from sklearn.externals import joblib
+from plotly.graph_objs import Bar, Heatmap
+from plotly import utils
+import joblib
 from sqlalchemy import create_engine
 
 
 app = Flask(__name__)
 
-def tokenize(text):
+def tokenize(text, stop_words=set(stopwords.words('english'))):
+    ''' Remove punctuation, strip unncessary spaces, lematize,
+    remove stopwords and tokenize text
+
+    Args:
+        text: str
+    returns:
+        clean_tokens: list of tokens
+    '''
+    text = text.lower()
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    text = re.sub(r'[0-9]+', '', text)
     tokens = word_tokenize(text)
+
+    filtered_tokens = [w for w in tokens if not w.lower() in stop_words]
+
     lemmatizer = WordNetLemmatizer()
 
     clean_tokens = []
-    for tok in tokens:
-        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
-        clean_tokens.append(clean_tok)
+    for token in filtered_tokens:
+        clean_token = lemmatizer.lemmatize(token).lower().strip()
+        clean_tokens.append(clean_token)
 
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+df = pd.read_sql_table('disaster_messages', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
 @app.route('/')
 @app.route('/index')
 def index():
-    
+
     # extract data needed for visuals
     # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
-    
+
+    features = df.iloc[:,3:].sum().sort_values(ascending=True)
+    features_counts = features.values
+    features_names = features.index
+
+    heatmap = df.iloc[:,3:].corr()
+
+    mask = np.triu(np.ones_like(heatmap, dtype=bool))
+    heatmap_LT = heatmap.mask(mask)
+    # features.corr()
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
     graphs = [
@@ -63,13 +91,56 @@ def index():
                     'title': "Genre"
                 }
             }
+        },
+        {
+            'data': [
+                Bar(
+                    x=features_counts,
+                    y=features_names,
+                    orientation='h',
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Message Categories',
+                'height': 700,
+                'margin': {
+                    'pad': 5
+                },
+                'yaxis': {
+                    'title': "Categories",
+                    'dtick': 1
+                },
+                'xaxis': {
+                    'title': "Count"
+                },
+            }
+        },
+        {
+            'data': [
+                Heatmap(
+                    x=features_names,
+                    y=features_names,
+                    z=heatmap_LT
+                )
+            ],
+
+            'layout': {
+                'title': 'Correlation of features in training dataset',
+                'height': 800,
+                'width': 800,
+                'xaxis_showgrid': False,
+                'yaxis_showgrid': False,
+    '           yaxis_autorange': 'reversed'
+            }
         }
     ]
-    
+
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
-    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
-    
+    graphJSON = json.dumps(graphs, cls=utils.PlotlyJSONEncoder)
+
+    print(graphJSON)
     # render web page with plotly graphs
     return render_template('master.html', ids=ids, graphJSON=graphJSON)
 
@@ -78,13 +149,13 @@ def index():
 @app.route('/go')
 def go():
     # save user input in query
-    query = request.args.get('query', '') 
+    query = request.args.get('query', '')
 
     # use model to predict classification for query
     classification_labels = model.predict([query])[0]
     classification_results = dict(zip(df.columns[4:], classification_labels))
 
-    # This will render the go.html Please see that file. 
+    # This will render the go.html Please see that file.
     return render_template(
         'go.html',
         query=query,
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,23 +1,136 @@
 import sys
-
+import pandas as pd
+import string
+import pickle
+import re
+from sqlalchemy import create_engine
+from nltk import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import precision_recall_fscore_support as score
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import classification_report
 
 def load_data(database_filepath):
-    pass
+    '''
+    Loads the data from the sql database in database_filepath
+    return feature as X and results as y
+
+    Args:
+        database_filepath: path to the sql database
+
+    returns:
+        X: panda dataframe : feature: list of messages
+        y: panda dataframe : result: classification of each message
+        category_names: list of categories
+    '''
+    # load data from database
+    engine = create_engine('sqlite:///{}'.format(database_filepath))
+    df = pd.read_sql_table('disaster_messages', engine)
+
+    # As feature is used the messages (translated in english)
+    X = df['message']
+    # classification for each message
+    y = df.iloc[:, 3:]
+
+    return X, y, y.columns
 
 
-def tokenize(text):
-    pass
+def tokenize(text, stop_words=set(stopwords.words('english'))):
+    ''' Remove punctuation, strip unncessary spaces, lematize,
+    remove stopwords and tokenize text
+
+    Args:
+        text: str
+    returns:
+        clean_tokens: list of tokens
+    '''
+    text = text.lower()
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    text = re.sub(r'[0-9]+', '', text)
+    tokens = word_tokenize(text)
+
+    filtered_tokens = [w for w in tokens if not w.lower() in stop_words]
+
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for token in filtered_tokens:
+        clean_token = lemmatizer.lemmatize(token).lower().strip()
+        clean_tokens.append(clean_token)
+
+    return clean_tokens
 
 
 def build_model():
-    pass
+    '''
+    build the model
+
+    Args: None
+
+    returns:
+        pipeline
+    '''
+    pipeline = Pipeline([
+                    ('vect',
+                    CountVectorizer(tokenizer=tokenize, ngram_range= (1, 2))),
+                    ('tfidf', TfidfTransformer()),
+                    ('clf', MultiOutputClassifier(
+                    RandomForestClassifier(min_samples_split=2, n_estimators=20)))
+                    ])
+
+    return pipeline
 
 
-def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+def evaluate_model(model, X_test, y_test, category_names):
+    ''' calculate the f1 score for each category and overall average
+    Args:
+        model: trained model
+        X_test: test data
+        y_test: expected output for X_test
+        category_names: list of categories / features
+    Return:
+        print f1 score for categories and average over all categories
+        df_results: pandas dataframe with all results
+    '''
+    # make model predictions
+    y_pred = model.predict(X_test)
+
+    # Creating a dictionary to store results
+    results = {}
+
+    # looping though all categories an storing their precision, recall and f1_score
+    for i in range(0, 36):
+        precision, recall, fscore, support = score(y_test.iloc[:,i],y_pred[:,i],average='weighted')
+        # adding results to the dictionary
+        results[category_names[i]] = [precision, recall, fscore]
+
+    # Creating pandas dataframe with results
+    df_results = pd.DataFrame.from_dict(results, orient='index', columns=['precision', 'recall', 'fscore'])
+
+    # printing overall modell performance
+    print('overall f1_score: {}\n'.format(df_results.fscore.mean()))
+
+    # printing category results
+    print(df_results)
+
+    return df_results
 
 
 def save_model(model, model_filepath):
+    ''' save the model to disk
+    Arg:
+        model: Machine learning model
+        model_filepath: str path to file
+    returns: None
+    '''
+    # save the model to dish to model_filepath
+    pickle.dump(model, open(model_filepath, 'wb'))
+
     pass
 
 
@@ -27,13 +140,13 @@ def main():
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-        
+
         print('Building model...')
         model = build_model()
-        
+
         print('Training model...')
         model.fit(X_train, Y_train)
-        
+
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
 
